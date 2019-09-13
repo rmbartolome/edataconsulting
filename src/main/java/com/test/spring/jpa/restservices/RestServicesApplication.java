21cfe400f1c3023b6283e18d49341c6456f9038c
Roberto Bartolome <rbartolome@cgtscorp.com>
2019-09-13 16:27:16 -0400

    rbartolome/runner bean

--- src/main/java/com/test/spring/jpa/restservices/RestServicesApplication.java
+++ src/main/java/com/test/spring/jpa/restservices/RestServicesApplication.java
@@ -2,8 +2,10 @@ package com.test.spring.jpa.restservices;
 
 import com.test.spring.jpa.restservices.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -17,6 +19,15 @@ public class RestServicesApplication {
 		SpringApplication.run(RestServicesApplication.class, args);
 	}
 
+
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+		//Save demo data after start
+
+		};
+	}
+}
 
 
 }
